2aaf582b6ca8f99e04dd38673362bd258514fa7b
Matthew Reider <matthew.reider@dynatrace.com>
2020-10-21 15:12:58 +0200

    next bit

--- dt-on-k8s/intro.sh
+++ dt-on-k8s/intro.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-spinner()
-{
-    local pid=$1
-    local delay=0.75
-    local spinstr='|/-\'
-    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-        local temp=${spinstr#?}
-        printf " [%c]  " "$spinstr"
-        local spinstr=$temp${spinstr%"$temp"}
-        sleep $delay
-        printf "\b\b\b\b\b\b"
-    done
-    printf "    \b\b\b\b"
-}
-
 echo '\u001b[32mPSCreating a k8s cluster...\u001b[m\r\n'
 launch.sh &
 spinner $!
